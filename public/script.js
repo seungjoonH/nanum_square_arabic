bef828c61a859641fc01cd73d153aa9a04bd8444
seungjoonH <hsj6831@gmail.com>
2024-10-18 17:56:15 +0900

    [Add] style.css file added

--- public/script.js
+++ public/script.js
@@ -18,15 +18,29 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  const getLengthWithKoreanWeight = (word) => {
+    let length = 0;
+  
+    for (let char of word) 
+      length += (1 + /[\uac00-\ud7af]/.test(char));
+  
+    console.log(word, length);
+    return length;
+  }
+
   let debouncer;
   const inputListener = async (event) => {
     const koreanWord = event.target.value;
+    if (getLengthWithKoreanWeight(koreanWord) > 50) {
+      korean.value = koreanWord.substring(0, koreanWord.length - 1);
+      return;
+    }
 
     clearTimeout(debouncer);
 
     debouncer = setTimeout(async () => {
       const translatedWord = await translate(koreanWord);
-      arabic.value = translatedWord;
+      arabic.innerText = translatedWord;
     }, 500);
   };
 
