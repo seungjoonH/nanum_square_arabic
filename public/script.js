ac2a27bcb90b8cfc93722b76e5dcdfb4bfec2f09
Hajune Lee <hajune.lee@gmail.com>
2024-10-23 11:41:41 +0900

    timer 100s fix

--- public/script.js
+++ public/script.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', () => {
     debouncer = setTimeout(async () => {
       const translatedWord = await translate(koreanWord);
       arTextArea.value = translatedWord + '\n';
-      clearTimer = setTimeout(clearText, 1000000);
+      clearTimer = setTimeout(clearText, 100000);
     }, 500);
   };
 
